fd7b99c7845d5f6d3925bebeb0bb9bea6966fc3b
Nandini-code <71508655+Nandini-code@users.noreply.github.com>
2022-06-08 19:01:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// Java Script Object Notation
+var student={
+  roll_number:12,
+  name:"Lakshmi",
+  marks:[45,67,89,99],
+  place:"Chennai"
+};
+function setup() {
+  console.log(student.name);
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
